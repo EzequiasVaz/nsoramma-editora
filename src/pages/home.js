1fd43250b97cec12989eee7aba085b9c8a8b8184
EzequiasVaz <ezequiasvaz@hotmail.com>
2020-12-01 10:50:48 -0300

    Inicio do Projeto

---
+++ src/pages/home.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+
+function Home() {
+
+    return (
+
+        <div className ='container'>
+
+            <header>
+                
+                <nav className='menu'>
+
+                </nav>
+            
+            </header>
+
+            <div className='slide-area'>
+
+
+            </div>
+
+            <div className='lancamentos-area'>
+
+
+            </div>
+
+            <div className='destaque-area'>
+                
+            
+            </div>
+            
+
+            <footer>
+
+            </footer>
+
+
+        </div>
+
+    )
+
+}
+
+
+export default Home
